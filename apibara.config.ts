61ed5ba6948e165f7802b4dc60724dfb3a60a07f
askwhyharsh <askwhyharsh@gmail.com>
2025-06-06 00:53:50 +0530

    update: init indexer with postgres

---
+++ apibara.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "apibara/config";
+
+export default defineConfig({
+  runtimeConfig: {
+    connectionString: process.env["POSTGRES_CONNECTION_STRING"] ?? "memory://",
+    starknetVault: {
+      startingBlock: 0,
+      streamUrl: "https://sepolia.starknet.a5a.ch",
+    },
+  },
+  // indexer: {
+  //   events: [
+  //     {
+  //       name: "CycleStarted",
+  //       contractAddress: "0x03578348b88501c60eff778f6f72b70d7291d490ba155f4285a8c0a8c7dd0897",
+  //       eventType: "caddy_finance_contracts::bitcoin_vault::BitcoinVault::CycleStarted",
+  //     },
+  //     {
+  //       name: "Deposited",
+  //       contractAddress: "0x03578348b88501c60eff778f6f72b70d7291d490ba155f4285a8c0a8c7dd0897",
+  //       eventType: "caddy_finance_contracts::bitcoin_vault::BitcoinVault::Deposited",
+  //     },
+  //     {
+  //       name: "WithdrawalRequested",
+  //       contractAddress: "0x03578348b88501c60eff778f6f72b70d7291d490ba155f4285a8c0a8c7dd0897",
+  //       eventType: "caddy_finance_contracts::bitcoin_vault::BitcoinVault::WithdrawalRequested",
+  //     },
+  //   ],
+  // },
+});
