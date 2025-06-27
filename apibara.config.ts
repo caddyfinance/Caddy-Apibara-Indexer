09a174ded48a665585cef63e5c39632de7a00eb2
askwhyharsh <askwhyharsh@gmail.com>
2025-06-27 23:32:33 +0700

    mainet: indexer

--- apibara.config.ts
+++ apibara.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   runtimeConfig: {
     connectionString: process.env["MONGODB_CONNECTION_STRING"] ?? "mongodb://localhost:27017/",
     starknetVault: {
-      startingBlock: process.env["STARTING_BLOCK"] ?? 847235,
+      startingBlock: process.env["STARTING_BLOCK"] ?? 1526756,
       streamUrl: process.env["STREAM_URL"] ?? "https://mainnet.starknet.a5a.ch",
       dbName: process.env["MONGODB_DB_NAME"] ?? "test_starknetVault_v1",
     },
